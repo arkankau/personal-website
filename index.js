07d294462bf54d2f07ac368af18717368478c1c2
arkankau <arkan.fadhil25@gmail.com>
2025-03-12 01:22:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// Description: This file contains the javascript code for the index.html file.
+
+// Function to open the tab
+var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+// This function is called when the user clicks on the menu icon.
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
